99fc59966090cae963d96817d5e1a1712942cfaf
hirushiRathnayake <hirushir99@gmail.com>
2023-11-05 14:42:55 +0530

    add content to contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-      
+      <h2>Here to help</h2>
+      <div>
+        <p>
+        Your feedback and enquiry is important to us, so we will endeavour to respond to you at our earliest. Your feedback will help us continuously improve ourselves to make it better for you and our other valued Customers.
+
+In the meantime if your enquiry is urgent and you require immediate assistance, our Customer Care team is here to assist you (Refer below for other ways to contact us)  
+        </p>
+      </div>
     </div>
   )
 }
