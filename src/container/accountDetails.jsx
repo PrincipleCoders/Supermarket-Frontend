9dc5f9e170e488c513b67e27ba500317fa85c457
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-11-06 01:23:15 +0530

    cart completed

--- src/container/accountDetails.jsx
+++ src/container/accountDetails.jsx
@@ -1,24 +1,32 @@
-
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-import { useState } from 'react';
-import '../styles/AccountDetails.css'
-
-
+import '../styles/AccountDetails.css';
 
 const AccountDetails = (props) => {
-
     const [fname, setfName] = useState(props.fname);
     const [lname, setlName] = useState(props.lname);
     const [address, setAddress] = useState(props.address);
     const [email, setEmail] = useState(props.email);
     const [telephone, setTelephone] = useState(props.telephone);
 
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setIsDialogOpen(true);
+    };
+
+    const handleConfirm = () => {
+        // Handle changes here and close the dialog
+        console.log('Confirmed changes');
+        setIsDialogOpen(false);
         console.log('First Name:', fname);
         console.log('Last Name:', lname);
         console.log('Email Address:', email);
@@ -27,6 +35,11 @@ const AccountDetails = (props) => {
 
     };
 
+    const handleCancel = () => {
+        // Cancel changes and close the dialog
+        console.log('Canceled changes');
+        setIsDialogOpen(false);
+    };
 
     return (
         <div>
@@ -48,9 +61,7 @@ const AccountDetails = (props) => {
                             onChange={(e) => setlName(e.target.value)}
                             color='success'
                         />
-
                     </div>
-
                     <TextField
                         id="email"
                         type='email'
@@ -74,10 +85,24 @@ const AccountDetails = (props) => {
                         onChange={(e) => setAddress(e.target.value)}
                         color='success'
                     />
-
                     <Button type="submit" variant='contained' color='success' sx={{ backgroundColor: "#3bb77e", width: '150px' }}>Save Changes</Button>
                 </div>
             </form>
+
+            <Dialog open={isDialogOpen} onClose={handleCancel}>
+                <DialogTitle>Confirm Changes</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Are you sure you want to save the changes?</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancel} color="success">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleConfirm} color="success" variant='contained'>
+                        Yes
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
--- src/container/cart.jsx
+++ src/container/cart.jsx
@@ -1,16 +1,149 @@
-import React from "react";
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import React, { useState } from "react";
+import '../styles/cart.css'
 
-export default function Cart(props) {
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import { Button } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-    return(
-<div>
-    <h2>Your Cart</h2>
 
-</div>
+export default function Cart() {
+    const [cartItems, setCartItems] = useState([
+        { name: 'Coca Cola 1L', quantity: 5, price: 250, image: 'cocacola.png' },
+        { name: 'Chips', quantity: 3, price: 150, image: 'chips.png' },
+        { name: 'Pizza', quantity: 2, price: 400, image: 'pizza.png' },
+        { name: 'Apples', quantity: 8, price: 120, image: 'apples.png' },
+        { name: 'Bananas', quantity: 10, price: 80, image: 'bananas.png' },
+        { name: 'Burger', quantity: 2, price: 300, image: 'burger.png' },
+        { name: 'Ice Cream', quantity: 4, price: 200, image: 'icecream.png' },
+        { name: 'Cookies', quantity: 5, price: 100, image: 'cookies.png' },
+        { name: 'Milk', quantity: 6, price: 60, image: 'milk.png' },
+        { name: 'Toothpaste', quantity: 2, price: 50, image: 'toothpaste.png' },
+    ]);
 
+    const updateQuantity = (index, newQuantity) => {
+        const updatedItems = [...cartItems];
+        updatedItems[index].quantity = newQuantity;
+        setCartItems(updatedItems);
+    };
+
+    const removeItem = (index) => {
+        const updatedItems = [...cartItems];
+        updatedItems.splice(index, 1);
+        setCartItems(updatedItems);
+    };
+
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleConfirmOrder = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const handleConfirmYes = () => {
+        console.log("Updated Cart Items:", cartItems);
+        setOpenDialog(false);
+        setCartItems([]);
+    };
+
+
+    return (
+        <div>
+            <div className="cart-header">
+                <h2>Your Cart</h2>
+                {cartItems.length != 0 && <Button
+                    variant='contained'
+                    color='success'
+                    sx={{ backgroundColor: "#3bb77e", height: '35px' }}
+                    size="small"
+                    disabled={cartItems.length === 0}
+                    onClick={handleConfirmOrder}
+                >
+                    Confirm Order
+                </Button>}
+            </div>
+            <div className="cart-container">
+                {cartItems.length === 0 && <h3 className="product-name">Nothing Here :( <br/>Please add items to your cart.</h3>}
+                {cartItems.length != 0 && <table className="cart-tabel">
+                    <thead>
+                        <tr className="headding">
+                            <th>Product</th>
+                            <th style={{ textAlign: "left", paddingLeft: '25px' }}>Name</th>
+                            <th>Unit Price</th>
+                            <th>Quantity</th>
+                            <th>Sub total</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartItems.map((item, index) => (
+                            <tr key={item.name}>
+                                <td>
+                                    <img className='item-image-cart' src={`/src/assets/categoryh1.png`} alt={item.name} />
+                                </td>
+                                <td>
+                                    <h4 className="product-name">{item.name}</h4>
+                                </td>
+                                <td>
+                                    <h4 className="product-price">Rs.{item.price}</h4>
+                                </td>
+                                <td>
+                                    <TextField
+                                        type="number"
+                                        value={item.quantity}
+                                        onChange={(e) => {
+                                            const newQuantity = parseInt(e.target.value);
+                                            if (!isNaN(newQuantity) && newQuantity >= 1) {
+                                                updateQuantity(index, newQuantity);
+                                            }
+                                        }}
+                                        color='success'
+                                        sx={{ width: '70px' }}
+                                        size="small"
+                                    />
+                                </td>
+                                <td>
+                                    <h4 className="product-subtotal">Rs.{item.quantity * item.price}</h4>
+                                </td>
+                                <td>
+                                    <IconButton aria-label="delete" onClick={() => removeItem(index)}>
+                                        <RemoveCircleOutlineIcon fontSize="inherit" />
+                                    </IconButton>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>}
+            </div>
+
+            <Dialog
+                open={openDialog}
+                onClose={handleCloseDialog}
+            >
+                <DialogTitle>Are you sure?</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Do you want to confirm your order?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="success">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleConfirmYes} color="success" variant="contained">
+                        Yes
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+        </div>
     );
 }
