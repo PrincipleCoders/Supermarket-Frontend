ef44a8da585eec1d0643476d98bd681e52d7a287
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-06 22:31:56 +0530

    added snackbarAlert to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LoginReg from "./pages/LoginReg.jsx";
 import Home from "./pages/Home.jsx";
+import {useState} from "react";
+import SnackbarAlert from "./components/SnackbarAlert.jsx";
 
 export default function App() {
+    const [alertStatus, setAlertStatus] = useState({type:'success',message:'',open:false});
+
+    const showAlert = (message,type) => {
+        setAlertStatus({type:type, message:message, open:true});
+    }
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/login" element={<LoginReg/>} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <SnackbarAlert alertStatus={alertStatus} setAlertStatus={setAlertStatus}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home showAlert={showAlert}/>}/>
+                    <Route path="/login" element={<LoginReg showAlert={showAlert}/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
