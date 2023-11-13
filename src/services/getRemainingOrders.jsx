c1db494eb17dbca03b27dffd0a6837f59dce0157
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-14 05:25:27 +0530

    Updated getRemainingOrders

--- src/services/getRemainingOrders.jsx
+++ src/services/getRemainingOrders.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL + 'order/remaining/all';
+
 const GetRemainingOrders = async () => {
   try {
-    const token = localStorage.getItem('userToken');
-    const response = await axios.get('https://localhost:8081/remainingOrders', {
+    const token = localStorage.getItem('token');
+    const response = await axios.get(API_URL, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
