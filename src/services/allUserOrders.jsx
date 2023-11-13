e7e3517d913c69ca06476b1def0d60b8548bef0c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-14 03:01:25 +0530

    minor fix

--- src/services/allUserOrders.jsx
+++ src/services/allUserOrders.jsx
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL + 'order/user/';
 
 const GetUserOrders = async (userId) => {
     try {
-        const token = localStorage.getItem('userToken');
+        const token = localStorage.getItem('token');
         const response = await axios.get(API_URL + userId, {
             headers: {
                 Authorization: `Bearer ${token}`,
