d5d8f4ac1b1bb2b333b129ee16a9f4f840e69602
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-13 19:48:30 +0530

    changed api url

--- src/services/allProducts.jsx
+++ src/services/allProducts.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL + 'inventory/product/all';
+
 const AllProducts = async() => {
     try {
 
         const token = localStorage.getItem('userToken');
-        const response = await axios.get('http://localhost:8088/inventory/product/all', {
+        const response = await axios.get(API_URL, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
