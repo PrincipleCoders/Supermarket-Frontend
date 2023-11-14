23718581f7d6c06a6b065cb422462af45d2295aa
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-14 11:53:35 +0530

    Fix API URL typo in updateMarkToDeliver.jsx

--- src/services/updateMarkToDeliver.jsx
+++ src/services/updateMarkToDeliver.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL + 'deliver/markToDeliverStatus/';
+const API_URL = import.meta.env.VITE_API_URL + 'delivery/markToDeliverStatus/';
 
 const UpdateMarkToDeliver = async (orderId, markStatus) => {
   try {
